45ed477047f21fa1d720604faa3ccce7432a9494
qyz <1031016362@qq.com>
2024-08-02 17:37:06 +0800

    deploy

---
+++ assets/teach_index.md.BARjzjpg.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./chunks/framework.CEwqCk1c.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"teach/index.md","filePath":"teach/index.md"}'),n={name:"teach/index.md"};function c(r,o,s,d,i,p){return a(),t("div")}const f=e(n,[["render",c]]);export{m as __pageData,f as default};
